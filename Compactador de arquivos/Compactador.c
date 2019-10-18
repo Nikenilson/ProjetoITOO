f372b59df3fc6e67019af01468c60a3c52b0e5bc
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-10-18 19:52:01 -0300

    Comeco das paradas

---
+++ Compactador de arquivos/Compactador.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nomeArquivo[50];
+    char escolha;
+
+    printf("%s","Digite o nome do arquivo que sera compactado ou descompactado:\n" );
+    //fflush();
+
+    scanf("%s", &nomeArquivo);
+
+    printf("%s","Digite 1 para descompactar o arquivo, ou qualquer outra coisa para compactar " );
+    //fflush();
+    scanf("%c", $escolha);
+
+    if(escolha != 1) //compactar
+    {
+
+    }
+    else //descompactar
+    {
+
+    }
+
+
+
+
+    return 0;
+}

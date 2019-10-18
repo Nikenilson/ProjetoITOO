5d8082ad9066b7f278521e5e667ce045e421ddc2
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-10-18 20:10:30 -0300

    Adicionei os primeiros ifs e o ppt

--- Compactador de arquivos/Compactador.c
+++ Compactador de arquivos/Compactador.c
@@ -5,18 +5,51 @@ int main()
 {
     char nomeArquivo[50];
     char escolha;
+    FILE *arqEntrada;
 
     printf("%s","Digite o nome do arquivo que sera compactado ou descompactado:\n" );
     //fflush();
 
     scanf("%s", &nomeArquivo);
+    strcpy(nomeArquivo,nomeArquivo);
 
     printf("%s","Digite 1 para descompactar o arquivo, ou qualquer outra coisa para compactar " );
     //fflush();
-    scanf("%c", $escolha);
+    scanf("%c", &escolha);
 
     if(escolha != 1) //compactar
     {
+        if(arqEntrada = fopen(nomeArquivo,"%rb") == NULL)
+            printf("%s", "Esse arquivo nao existe");
+        else
+        {
+
+            /*1.
+            Scan text to be compressed and tally
+            occurrence of all characters.
+
+            2.
+            Sort or prioritize characters based on
+            number of occurrences in text.
+
+            3.
+            Build Huffman code tree based on
+            prioritized list.
+
+            4.
+            Perform a traversal of tree to determine
+            all code words.
+
+            5.
+            Scan text again and create new file
+            using the Huffman codes.
+            */
+
+            //for()//preendhe a fila de frequencia
+
+
+            fclose(arqEntrada);
+        }
 
     }
     else //descompactar

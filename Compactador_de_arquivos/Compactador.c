1b12ebd8309ed0528499cb00bff20f630b88a2fb
GugaH13 <cc18180@g.unicamp.br>
2019-10-22 08:35:07 -0300

    alola pre-alula-da-simone

---
+++ Compactador_de_arquivos/Compactador.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int escolha;
+
+    do
+    {
+        clearScreen();
+        puts("[Descompactador de Arquivos]\n");
+        puts("1. Compactar arquivo");
+        puts("2. Descompactar arquivo");
+        puts("3. Sair\n");
+        printf("%s", "Digite a opcao desejada: ");
+        fflush(stdout);
+
+        scanf("%d", &escolha);
+        fflush(stdin);
+
+        switch(escolha)
+        {
+            case 1:
+                compactar();
+                break;
+            case 2:
+                descompactar();
+                break;
+            case 3:
+                exit(0);
+                break;
+        }
+    }
+    while (escolha != 3);
+
+    return 0;
+}
+
+void compactar()
+{
+    clearScreen();
+
+    char nomeArquivo[256];
+    FILE *arqEntrada;
+
+    printf("%s", "Digite o nome do arquivo que sera compactado:\n" );
+    fflush(stdout);
+
+    gets(&nomeArquivo);
+    fflush(stdin);
+
+    if(arqEntrada = fopen(nomeArquivo,"rb") == NULL)
+        puts("Esse arquivo nao existe!");
+    else
+    {
+        int vetorFrequencia [256];
+
+        char aux = fgetc(arqEntrada);
+        while(aux != EOF)
+        {
+            aux = fgetc(arqEntrada);
+            vetorFrequencia[aux]++;
+        }
+
+        rewind(arqEntrada);
+
+        for(int i = 0; i < 256; i++){
+            printf("%d", vetorFrequencia[i]);
+            printf("%c", vetorFrequencia[i]);
+        }
+
+        /*
+        1.
+        Scan text to be compressed and tally
+        occurrence of all characters.
+
+        2.
+        Sort or prioritize characters based on
+        number of occurrences in text.
+
+        3.
+        Build Huffman code tree based on
+        prioritized list.
+
+        4.
+        Perform a traversal of tree to determine
+        all code words.
+
+        5.
+        Scan text again and create new file
+        using the Huffman codes.
+
+        //for()//preenche a fila de frequencia
+        */
+
+        fclose(arqEntrada);
+    }
+
+    system("PAUSE");
+}
+
+void descompactar()
+{
+    clearScreen();
+
+    char nomeArquivo[50];
+    FILE *arqEntrada;
+
+    printf("%s", "Digite o nome do arquivo que sera descompactado:\n" );
+    fflush(stdout);
+
+    scanf("%s", &nomeArquivo);
+    fflush(stdin);
+    strcpy(nomeArquivo, nomeArquivo);
+
+
+    if(arqEntrada = fopen(nomeArquivo,"rb") == NULL)
+        puts("Esse arquivo nao existe!");
+    else
+    {
+
+
+        fclose(arqEntrada);
+    }
+
+    system("PAUSE");
+}
+
+void clearScreen()
+{
+    system("@cls||clear");
+}

1198d878cfb10f8c90f207051fa79ea2931dc1a4
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-11-09 18:00:15 -0300

    Revert "tá um pouco triste, mas o pedrao vai ajudar"
    This reverts commit 14fce52b8c81e72d5b13be437c2e78aee157be0d.

--- CompactadorDeArquivos/Compactador.c
+++ CompactadorDeArquivos/Compactador.c
@@ -189,8 +189,6 @@ void compactar()
 
         /*AuxHuff agora eh a raiz da arvore*/
         auxHuff = (HuffNode*) desenfileirar(&fila.lis);
-        p(auxHuff);
-
 
         inicieLista(&lista);
 
@@ -322,7 +320,7 @@ void descompactar()
     else
     {
         char lixoMemoria    = 0;
-        char caracterLido   = 0;
+        int  caracterLido   = 0;
         int  qtdChars       = 0;
         int  auxChar        = 0;
         int  frequenciaLida = 0;
@@ -368,8 +366,8 @@ void descompactar()
             insiraEmOrdem(&fila.lis, novo, comparaHuffNode);
         }
 
+        puts("aqui");
         auxHuff = (HuffNode*) desenfileirar(&fila.lis);
-        p(auxHuff);
 
         inicieLista(&lista);
 
